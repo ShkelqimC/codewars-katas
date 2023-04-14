747855ef5e53c083e36b4764fd95e3d4cdb9b12f
Shkelqim Cakaj <shkelqimcakaj@hotmail.com>
2023-04-14 17:00:53 +0200

    Remove

--- codewars katas/Program.cs
+++ codewars katas/Program.cs
@@ -8,7 +8,7 @@
 
 
 
-Console.WriteLine(CountSheep(4));
+Console.WriteLine(Remove("Hi! Hi!! Hi!"));
 
 //Create Phone Number 6kyu
 //https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/csharp 
@@ -19,7 +19,25 @@ string CreatePhoneNumber(int[] numbers)
     return $"({numbers[0]}{numbers[1]}{numbers[2]}) {numbers[3]}{numbers[4]}{numbers[5]}-{numbers[6]}{numbers[7]}{numbers[8]}";
 }
 
+//Exclamation marks series #7: Remove words from the sentence if it contains one exclamation mark
+//https://www.codewars.com/kata/57fafb6d2b5314c839000195/train/csharp
+static string Remove(string s)
+{
+    var res = "";
+    var split = s.Split(' ');
 
+    foreach (var i in split)
+    {
+        var count = i.Count(k => (k == '!'));
+
+        if (count != 1) res += " " + i;
+    }
+    if (res.Length > 1)
+        res = res.Remove(0, 1);
+
+
+    return res;
+}
 
 //Find the index of the second occurrence of a letter in a string
 //https://www.codewars.com/kata/63f96036b15a210058300ca9/train/csharp
