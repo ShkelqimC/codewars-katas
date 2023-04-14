347d135cd3b3e506171bb13acfcb4582a1e60194
Shkelqim Cakaj <shkelqimcakaj@hotmail.com>
2023-04-14 16:08:54 +0200

    Add project files.

---
+++ codewars katas/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//Create Phone Number 6kyu
+//https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/csharp 
