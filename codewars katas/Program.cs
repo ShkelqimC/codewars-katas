0131e2161b521cfa6d5e3d467d4b3b828b879a9e
Shkelqim Cakaj <shkelqimcakaj@hotmail.com>
2023-04-16 15:37:25 +0200

    greed is good

--- codewars katas/Program.cs
+++ codewars katas/Program.cs
@@ -2,13 +2,107 @@
 
 
 
-var one = new List<int>() { 1, 1, 1, 2, 1, 1, 1 };
+using System.Security.Cryptography.X509Certificates;
 
-Console.WriteLine(GetUnique(one));
+var one = new int[] { 2, 4, 4, 5, 4 };
+
+Console.WriteLine(Score(one));
 
 
 
 Console.WriteLine(Calculator(".... // .."));
+//Greed is Good 5KYU
+//https://www.codewars.com/kata/5270d0d18625160ada0000e4/train/csharp
+int Score(int[] dice)
+{
+    var score = 0;
+    var group = dice.GroupBy(x => x);
+    foreach (var t in group)
+    {
+
+        var c = t.Count();
+            do
+            {
+                if (c >= 3)
+                {
+
+                    switch (t.Key)
+                    {
+                        case 1:
+                            score += 1000;
+                            c -= 3;
+                            break;
+                        case 2:
+                            score += 200;
+                            c -= 3;
+                            break;
+                        case 3:
+                            score += 300;
+                            c -= 3;
+                            break;
+                        case 4:
+                            score += 400;
+                            c -= 3;
+                            break;
+                        case 5:
+                            score += 500;
+                            c -= 3;
+                            break;
+                        case 6:
+                            score += 600;
+                            c -= 3;
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+                else if(t.Key == 5 || t.Key == 1)
+                {
+                    if (t.Key == 1 && c > 0)
+                    {
+                        score += 100;
+                        c -= 1;
+
+                    }
+                    if (t.Key == 5 && c > 0)
+                    {
+
+                        score += 50;
+                        c -= 1;
+                    }
+                }
+            else
+            {
+                c = 0;
+            }
+
+            } while (c != 0);
+
+
+        
+
+    }
+
+    return score;
+}
+//Count the smiley faces! 6kyu
+//https://www.codewars.com/kata/583203e6eb35d7980400002a/train/csharp
+static int CountSmileys(string[] smileys)
+{
+    if (!smileys.Any()) return 0;
+    var res = 0;
+    foreach (var s in smileys)
+    {
+        if (s.Contains(" ")) continue;
+
+        if ((s.Contains(')') || s.Contains('D')) && (s.Contains(':') || s.Contains(';')))
+        {
+            res++;
+        }
+    }
+    return res;
+}
 //Find the unique number 6kyu
 //https://www.codewars.com/kata/585d7d5adb20cf33cb000235
 int GetUnique(IEnumerable<int> numbers)
@@ -38,7 +132,7 @@ string Calculator(string txt)
     string[] symbols = new[] { "+", "-", "*", "/" };
     string sym = "";
     foreach (var s in symbols)
-     {
+    {
         var index = txt.IndexOf(s);
 
         if (index >= 0) sym = s;
@@ -53,28 +147,49 @@ string Calculator(string txt)
         case "-":
             count = (split[0].Length - 1) - (split[1].Length - 1);
             break;
-            case "*":
+        case "*":
             count = (split[0].Length - 1) * (split[1].Length - 1);
             break;
-            case "/":
+        case "/":
             count = (split[0].Length - 1) / (split[2].Length - 1);
             break;
         default:
             break;
     }
     var res = "";
-    if(count > 0)
+    if (count > 0)
     {
 
-    for (int i = 0; i < count; i++)
-    {
-        res += ".";
-    }
+        for (int i = 0; i < count; i++)
+        {
+            res += ".";
+        }
     }
 
     return res;
 }
+//Find the stray number
+//https://www.codewars.com/kata/57f609022f4d534f05000024/train/csharp
+int Stray(int[] numbers)
+{
 
+    return numbers[numbers.Select(i => numbers.Where(k => k == i).Count() == 1).ToList().IndexOf(true)];
+}
+
+//Don't give me five! 7kyu
+//https://www.codewars.com/kata/5813d19765d81c592200001a/train/csharp
+static int DontGiveMeFive(int start, int end)
+{
+    var res = new List<int>();
+    for (int i = start; i <= end; i++)
+    {
+        if (!i.ToString().Contains("5"))
+        {
+            res.Add(i);
+        }
+    }
+    return res.Count();
+}
 //Power of two 7kyu
 //https://www.codewars.com/kata/534d0a229345375d520006a0/train/csharp
 static bool PowerOfTwo(int n)
@@ -135,7 +250,7 @@ static double Square(double a)
 //https://www.codewars.com/kata/5aa736a455f906981800360d/train/csharp
 static bool Feast(string beast, string dish)
 {
-    return (beast[0] == dish[0] && beast[beast.Length-1] == dish[dish.Length-1]);
+    return (beast[0] == dish[0] && beast[beast.Length - 1] == dish[dish.Length - 1]);
 }
 
 //If you can't sleep, just count sheep!! 8kyu
